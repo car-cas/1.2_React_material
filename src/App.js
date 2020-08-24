ad8199ced18cae676778889d358fe48a74f2a843
car-cas <carlos.castaneda-l@mail.escuelaing.edu.co>
2020-08-24 16:01:09 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TodoList} from "./components/TodoList";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from "moment";
+import {Login} from "./components/Login.js"
+import {TodoApp} from "./components/TodoApp.js"
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isLoggedIn : false};
+    }
+
+    render() {
+        const LoginView = () => (
+            <Login/>
+        );
+        const TodoAppView = () => (
+            <TodoApp/>
+        );
+        return (
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">TODO React App</h1>
+                    </header>
+
+                    <br/>
+                    <br/>
+
+                    <ul>
+                        <li><Link to="/">Login</Link></li>
+                        <li><Link to="/todo">Todo</Link></li>
+                    </ul>
+
+                    <div>
+                        <Route exact path="/" component={LoginView}/>
+                        <Route path="/todo" component={TodoAppView}/>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
